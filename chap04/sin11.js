5f947d3b1ec6b28d7b2d960211715417c95f6724
Hiroshi Suda <suda@SudanoMacBook-Air.local>
2022-08-29 18:27:51 +0900

    chap04まで完成

--- chap04/sin11.js
+++ chap04/sin11.js
@@ -4,26 +4,19 @@ const arr = audioCtx.createBuffer(2, audioCtx.sampleRate, audioCtx.sampleRate); 
 
 let chanL = arr.getChannelData( 0 );
 let chanR = arr.getChannelData( 1 );
-let dataL = new Float32Array(audioCtx.sampleRate);
-let dataR = new Float32Array(audioCtx.sampleRate);
 
 let f = 440.0;
 for( let x = 0; x<audioCtx.sampleRate; x++ ) {
-    let y = Math.sin( x * f / audioCtx.sampleRate * 2.0 * Math.PI );
-    dataL[x] = y;
-    dataR[x] = y;
+    let y = Math.sin( x * f * 2.0 * Math.PI / audioCtx.sampleRate );
+    chanL[x] = y;
+    chanR[x] = y;
 }
-chanL.set(dataL);
-chanR.set(dataR);
-const source = audioCtx.createBufferSource();
-source.bufer = arr;
-source.loop = false;
-source.loopStart = 0;
-source.loopEnd = audioCtx.sampleRate;
-source.playbackRate.value = 1.0;
-source.connect( audioCtx.destination );
 
 document.querySelector( '#btn1' ).addEventListener( 'click', () => {
-    source.start(0);
+    const source = audioCtx.createBufferSource();
+    source.buffer = arr;
+    source.connect( audioCtx.destination );
+
+    source.start();
     console.log("start");
 });
