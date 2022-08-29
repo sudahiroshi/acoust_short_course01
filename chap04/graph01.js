6bc483b4ba0fd6d06cd018c2726c2b1f61a913c2
Hiroshi Suda <suda@SudanoMacBook-Air.local>
2022-08-29 18:46:58 +0900

    beginPathを追加

--- chap04/graph01.js
+++ chap04/graph01.js
@@ -3,6 +3,7 @@ let graphicsCtx = element.getContext( '2d' );
 
 document.querySelector('#btn2').addEventListener( 'click', () => {
     console.log( "graph" );
+    graphicsCtx.beginPath();
     graphicsCtx.moveTo( 0, 400 );
     for( let x=1; x<element.width; x++ ) {
         graphicsCtx.lineTo( x, 200-(chanL[x*64]*200) );
